ff786f21ad84252e4d3efbbf20c99e410e9c145d
MyNameIs3rik <erikkocurek@gmail.com>
2026-01-28 12:27:48 +0100

    added processing

---
+++ processing/clean.py
@@ -0,0 +1,39 @@
+def clean_price(price):
+    if price is None:
+        return None
+    currency = ['$', '£', '€']
+    for symbol in currency:
+        price = price.replace(symbol, '')
+    return float(price.strip())
+
+def clean_stock(stock):
+    if stock is None:
+        return False
+    if stock.lower().strip() == 'in stock':
+        return True
+    return False
+
+def clean_rating(rating):
+    ratings_map = {
+        'One': 1,
+        'Two': 2,
+        'Three': 3,
+        'Four': 4,
+        'Five': 5
+    }
+    return ratings_map.get(rating, 0)
+
+
+def clean_book(book):
+    return {
+        'title': book['title'],
+        'price': clean_price(book['price']),
+        'stock': clean_stock(book['stock']),
+        'rating': clean_rating(book['rating'])
+    }
+
+def clean_data(data):
+    return [clean_book(book) for book in data]
+
+if __name__ == "__main__":
+    pass
